5ef31dd5c4a18a4134c1acca96ae850e20c97bc0
tanxch2002 <76683370+tanxch2002@users.noreply.github.com>
2022-08-01 18:54:19 +0800

    第二次作业

---
+++ Test/Student.m
@@ -0,0 +1,19 @@
+//
+//  student.m
+//  Test
+//
+//  Created by tanxch on 2022/8/1.
+//
+
+#import <Foundation/Foundation.h>
+#import "Student.h"
+
+@implementation Student
+
+- (void)study:(float)time
+{
+    NSLog(@"%f", time);
+}
+
+@end
+
